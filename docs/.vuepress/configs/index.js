fd958636ff28e8cb8d962d7c3a610afd6bbf06ff
alexzhang <1642114555@qq.com>
2021-12-23 17:30:38 +0800

    delopy gh_pages

--- docs/.vuepress/configs/index.js
+++
@@ -1,3 +0,0 @@
-const navbar = require('./sidebar/navbar')
-
-exports.navbar = navbar
--- docs/.vuepress/configs/sidebar/navbar.js
+++
@@ -1,7 +0,0 @@
-const navbar = [
-  { text: 'Vue 3 基础语法', link: '/01-Vue3/' },
-  { text: 'Vue Router', link: '/02-VueRouter/' },
-  { text: 'Pinia', link: '/03-Pinia/' },
-]
-
-module.exports = navbar
